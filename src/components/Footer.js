1f291528be81b0a2eb8fc64167cc9782ede60c8b
Kirill <117633274+04Fox04@users.noreply.github.com>
2023-06-13 18:29:39 +0300

    uuu

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import "../index.css";
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__copyright">© 2020 Mesto Russia Kirill S.</p>
+    </div>
+  );
+};
+
+export default Footer
